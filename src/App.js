a37d54430c4e12eb029c80602df072df5e2db963
Jason <jasonutama0206@gmail.com>
2022-07-21 14:07:11 -0400

    Verion 1 commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
-import Header from './components/Header'
-import FeedbackData from './Data/FeedbackData'
-import FeedbackList from './components/FeedbackList'
+import { v4 as uuidv4 } from 'uuid'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { useState } from 'react'
+import Header from './components/Header'
+import FeedbackList from './components/FeedbackList'
+import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
+import AboutPage from './Pages/AboutPage'
+import AboutIconLink from './components/AboutIconLink'
+import FeedbackData from './Data/FeedbackData'
 
 function App() {
-  const[feedback,setFeedback]=useState(
-    FeedbackData)
+  const [feedback, setFeedback] = useState(FeedbackData)
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback, ...feedback])
+  }
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to remove item?')) {
+      setFeedback(feedback.filter((item) => item.id !== id))
+    }
+  }
   return (
-    <>
+    <Router>
       <Header />
       <div className="container">
-        <FeedbackList feedback={feedback}/>
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+              </>
+            }
+          ></Route>
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+        <AboutIconLink />
       </div>
-    </>
+    </Router>
   )
 }
 
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,11 +1,19 @@
 import Card from './Shared/Card'
+import { FaTimes } from 'react-icons/fa'
 import PropTypes from 'prop-types'
 import FeedbackData from '../Data/FeedbackData'
-function FeedbackItem({ item }) {
+function FeedbackItem({ item, handleDelete }) {
+  // const handleClick = (id) => {
+  //   console.log(id)
+  // }
+
   return (
     <Card>
-      <div className='num-display'>{item.rating}</div>
-      <div className='text-display'>{item.text}</div>
+      <div className="num-display">{item.rating}</div>
+      <button className="close" onClick={()=>handleDelete(item.id)}>
+        <FaTimes color="purple" />
+      </button>
+      <div className="text-display">{item.text}</div>
     </Card>
   )
 }
